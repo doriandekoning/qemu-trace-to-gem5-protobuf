1659009a77b77131f2363413eea5486326de1a36
Dorian de Koning <doriandekoning@gmail.com>
2019-04-10 11:23:43 +0200

    Add reading header

---
+++ convert.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdint.h>
+
+static FILE *trace_fp;
+
+
+typedef struct {
+    uint64_t header_event_id; /* HEADER_EVENT_ID */
+    uint64_t header_magic;    /* HEADER_MAGIC    */
+    uint64_t header_version;  /* HEADER_VERSION  */
+} TraceLogHeader;
+
+int read_header() {
+
+	TraceLogHeader header = {
+		.header_event_id  = 0,
+		.header_magic  = 0,
+		.header_version = 0,
+	};
+
+
+	if (fread(&header, sizeof header, 1, trace_fp) != 1) {
+		return 1;
+	}
+
+	printf("Header event id: %llu\n", header.header_event_id);
+	printf("Header magic: %llx\n", header.header_magic);
+	printf("Header version: %llu\n", header.header_version);
+
+	return 0;
+}
+
+int main() {
+	// Open file
+	trace_fp = fopen("tracefile", "r");
+	if (!trace_fp){
+		printf("Could not open file\n");
+		return -1;
+	}
+
+	if(read_header()) {
+		printf("Unable to read header\n");
+	}
+	// Close file
+	fclose(trace_fp);
+}
