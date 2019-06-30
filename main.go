2e3d62f0328f144358d09c9ee85b0f041e099e06
Dorian de Koning <dorian@weave.nl>
2019-06-30 14:39:41 +0200

    Add option to not split the output for each cpu id

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"encoding/binary"
 	"bufio"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"os"
+
 	pb "github.com/doriandekoning/qemu-trace-to-gem5-protobuf/messages"
 	"github.com/gogo/protobuf/proto"
 )
@@ -26,6 +27,7 @@ var cpus map[uint64]int
 var outFileBasePath string
 var largestTick uint64
 var outFiles []*os.File
+var split bool
 
 func main() {
 	inFilePath := os.Args[1]
@@ -38,6 +40,12 @@ func main() {
 	if outFileBasePath == "" {
 		fmt.Println("No output file is provided")
 	}
+
+	split := (len(os.Args) > 3 && os.Args[3] != "false")
+	if !split {
+		fmt.Println("Not splitting output")
+	}
+
 	fmt.Println("Using output file:", os.Args[2])
 	cpus = make(map[uint64]int)
 	inFile, err := os.Open(inFilePath)
@@ -54,7 +62,7 @@ func main() {
 	mappingFilePath := outFileBasePath + ".mapping"
 	mappingFile, err := os.Create(mappingFilePath)
 	if err != nil {
-	    panic(err)
+		panic(err)
 	}
 	defer mappingFile.Close()
 	defer closeOutFiles()
@@ -68,13 +76,18 @@ func main() {
 		} else if recordType == 1 {
 			packet, cpuId := readTraceEvent(inFile)
 			if packet == nil {
-			    continue
+				continue
 			}
 			marshaledPacket, err := proto.Marshal(packet)
 			if err != nil {
 				panic(err)
 			}
 			lengthVarint := proto.EncodeVarint(uint64(len(marshaledPacket)))
+			if split {
+				cpuIdUint := uint64(cpuId)
+				packet.CpuID = &cpuIdUint
+				cpuId = 0
+			}
 			outFiles[cpuId].Write(lengthVarint)
 			outFiles[cpuId].Write(marshaledPacket)
 		} else {
@@ -99,7 +112,7 @@ func main() {
 }
 
 func closeOutFiles() {
-	for _, file :=  range outFiles {
+	for _, file := range outFiles {
 		file.Close()
 	}
 }
@@ -164,7 +177,7 @@ func readTraceHeader(file *os.File) {
 
 func readTraceEvent(file *os.File) (*pb.Packet, int) {
 	eventID := readUint64(file)
-	if eventID != 75  && eventID != uint64(0xfffffffffffffffe){
+	if eventID != 75 && eventID != uint64(0xfffffffffffffffe) {
 		mesg := fmt.Sprintf("Only traces with eventID 75 are supported, found event with id: %d\n", eventID)
 		panic(mesg)
 	}
@@ -177,9 +190,9 @@ func readTraceEvent(file *os.File) (*pb.Packet, int) {
 	timestampInTicks := ticksPerNS * (timestamp - startTimestamp)
 	largestTick = timestampInTicks
 	recLen := readUint32(file)
-	readUint32(file) // tracePid	
+	readUint32(file) // tracePid
 	if eventID == uint64(0xfffffffffffffffe) {
-//		readBytes(file, int(recLen))
+		//		readBytes(file, int(recLen))
 		fmt.Println("Found dropped trace event", readUint64(file))
 		return nil, -1
 	}
@@ -188,13 +201,13 @@ func readTraceEvent(file *os.File) (*pb.Packet, int) {
 	var cpu int
 	var ok bool
 	if cpu, ok = cpus[qemuCpu]; !ok {
-	    cpus[qemuCpu] = len(outFiles)
-	    outFiles = append(outFiles, createOutfile(len(outFiles)))
+		cpus[qemuCpu] = len(outFiles)
+		outFiles = append(outFiles, createOutfile(len(outFiles)))
 	}
 	vaddr := readUint64(file)
-	vaddr = vaddr%2147483648
-	info :=	readUint64(file)
-	qemuCmd :=  ((info >> 5)  & 0x1) // Last bit of info is 1 if it is a store operation
+	vaddr = vaddr % 2147483648
+	info := readUint64(file)
+	qemuCmd := ((info >> 5) & 0x1) // Last bit of info is 1 if it is a store operation
 	var cmd uint32
 	if qemuCmd == 0 {
 		cmd = 1
@@ -212,12 +225,11 @@ func readEventMapping(file *os.File, mappingFile *os.File) {
 	length := readUint32(file)
 	eventName := string(readBytes(file, int(length)))
 
-	_, err := mappingFile.Write([]byte(fmt.Sprintf( "%d:%s\n", eventID, eventName)))
+	_, err := mappingFile.Write([]byte(fmt.Sprintf("%d:%s\n", eventID, eventName)))
 	if err != nil {
-	    panic(err)
+		panic(err)
 	}
 }
-
 
 func readUint64(file *os.File) uint64 {
 	return binary.LittleEndian.Uint64(readBytes(file, 8))
@@ -228,24 +240,23 @@ func readUint32(file *os.File) uint32 {
 	return binary.LittleEndian.Uint32(readBytes(file, 4))
 }
 
-
 func readBytes(file *os.File, amount int) []byte {
-	if uint64(amount) <= uint64(len(buffer)) - bufferIndex {
+	if uint64(amount) <= uint64(len(buffer))-bufferIndex {
 		bufferIndex += uint64(amount)
-		return buffer[(bufferIndex-uint64(amount)):bufferIndex]
+		return buffer[(bufferIndex - uint64(amount)):bufferIndex]
 	}
 	//Not enough bytes in buffer
 	fileOffset += bufferIndex
 	if len(buffer) == 0 {
-	    buffer = make([]byte, 100000000)
+		buffer = make([]byte, 100000000)
 	}
 	fmt.Printf("%0.1f%%\n", 100*float64(fileOffset)/float64(fileSize))
 	_, err := file.ReadAt(buffer, int64(fileOffset))
 	if err == io.EOF {
 		fmt.Println("End of file found")
-	        fmt.Println("Reads:", totalReads)
-	        fmt.Println("Writes:", totalWrites)
-	        fmt.Println("LargestTick:", largestTick)
+		fmt.Println("Reads:", totalReads)
+		fmt.Println("Writes:", totalWrites)
+		fmt.Println("LargestTick:", largestTick)
 		closeOutFiles()
 		os.Exit(0)
 	} else if err != nil {
